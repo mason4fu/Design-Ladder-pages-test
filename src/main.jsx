3fb5b5da2051ab1e4f8cb74c198170e675b2bc78
mason4fu <masonfu@outlook.com>
2025-11-13 09:40:34 -0600

    Fix GitHub Pages routing by adding basename to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Design-Ladder-pages-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
